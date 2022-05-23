2449982b2ae924248dc2d5c7296a41b6e5f5037e
李有为 <willtrump@outlook.com>
2022-05-23 20:35:44 +0800

    2022年5月23日

---
+++ 2022-5-23 ButtonStyle-TrumpX/ButtonStyle-TrumpX/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonStyle_TrumpX
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+$@"标题：按钮美化
+作者：Trump-X
+时间：2022年5月23日 19点51分
+地点：中国广东省珠海市斗门区翠湖苑
+"
+                ) ;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# WpfVideo
+# 2022-5-23 ButtonStyle-TrumpX
+效果展示
+
+<img src="D:\OneDrive\mdimg\17点13分 2022年4月10日-16533091850541.gif" alt="17点13分 2022年4月10日" style="zoom: 50%;" />
+
+B站地址
+
+[按钮美化_哔哩哔哩_bilibili](https://www.bilibili.com/video/BV1HT4y1q759/)
+
+油管地址：
+
+[(5) 按鈕美化 beautiful button 2022-5-23 - YouTube](https://www.youtube.com/watch?v=mzMwi_q-Neg)
+
+
+
