6334bd83619eb9666e4398163cc13c2a29061218
李有为 <willtrump@outlook.com>
2022-05-25 07:58:52 +0800

    Add CheckBoxStyle

---
+++ 2022-5-25 CheckBoxStyle-TrumpX/CheckBox-Blend2/CheckBoxStyleHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace CheckBox_Blend2
+{
+    public class CheckBoxStyleHelper : DependencyObject
+    {
+        public static double GetIconSize(DependencyObject obj)
+        {
+            return (double)obj.GetValue(IconSizeProperty);
+        }
+
+        public static void SetIconSize(DependencyObject obj, double value)
+        {
+            obj.SetValue(IconSizeProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconSizeProperty =
+            DependencyProperty.RegisterAttached("IconSize", typeof(double), typeof(CheckBoxStyleHelper), new PropertyMetadata(30d));
+    }
+}
---
+++ 2022-5-25 CheckBoxStyle-TrumpX/CheckBox-Blend2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBox_Blend2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+$@"
+标题：复选框美化（beautiful checkbox）
+作者：TrumpX
+时间：2022年5月25日 05点20分
+地点：中国广东省珠海市斗门区翠湖苑
+"
+                );
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show(
+$@"
+标题：复选框美化（beautiful checkbox）
+作者：TrumpX
+时间：2022年5月25日 05点20分
+地点：中国广东省珠海市斗门区翠湖苑
+"
+                );
+        }
+    }
+}
